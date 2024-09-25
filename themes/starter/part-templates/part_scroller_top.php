d4fcedf23ed0c616a292914310d1011e6b08d00b
Limanskyi1 <125376956+Limanskyi1@users.noreply.github.com>
2024-09-25 18:52:59 +0300

    Delete themes directory

--- themes/starter/part-templates/part_scroller_top.php
+++
@@ -1,6 +0,0 @@
-<svg id="progressCircle" viewBox="0 0 100 100">
-    <circle id="outerCircle" r="40" cx="50" cy="50" fill="transparent" stroke-width="5px"></circle>
-    <circle id="innerCircle" r="40" cx="50" cy="50" fill="transparent" stroke-width="5px" stroke-dasharray="0, 251.2"
-        transform="rotate(-90 50 50)"></circle>
-    <text x="50" y="55" text-anchor="middle" font-size="20">0%</text>
-</svg>
